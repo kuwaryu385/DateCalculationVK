8cbc77a9fb29d6f9d4fac8b216fcc8b2517d886c
Ryuta Kuwamura <staticjbulejass@gmail.com>
2020-12-18 15:29:49 +0900

    GradleにてSpringBootプロジェクト作成

---
+++ DateCalculationVK-1/src/main/java/com/example/dateapp/DateCalculationVk1Application.java
@@ -0,0 +1,13 @@
+package com.example.dateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateCalculationVk1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateCalculationVk1Application.class, args);
+	}
+
+}
---
+++ DateCalculationVK-1/src/test/java/com/example/dateapp/DateCalculationVk1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateCalculationVk1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
