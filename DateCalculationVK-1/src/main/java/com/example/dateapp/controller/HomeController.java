5d8f8d6625ea2094cdd40ed4aefd0b6057480960
Ryuta Kuwamura <staticjbulejass@gmail.com>
2020-12-20 21:19:41 +0900

    Revert "Revert "Home、新規作成画面の実装""
    This reverts commit 1a8e0b529baaa8ae0a67d0990a9063467a86afe3.

---
+++ DateCalculationVK-1/src/main/java/com/example/dateapp/controller/HomeController.java
@@ -0,0 +1,75 @@
+package com.example.dateapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//import com.example.firstcrudapp.domein.MyBooksModel;
+//import com.example.firstcrudapp.service.MyBooksService;
+
+@Controller
+@RequestMapping("/") //Home画面の表示
+public class HomeController {
+
+	//テーブル一覧画面の表示　ReadAll処理
+	@GetMapping
+	public String Home(Model model) {
+		return "home";
+	}
+
+	//		//本のIDにて、本の内容を取得
+	//		@GetMapping("{id}")
+	//		public String show(@PathVariable Integer id, Model model) {
+	//			model.addAttribute("mybook", myBooksService.selectOne(id));
+	//			return "show";
+	//		}
+
+	//	新規作成画面の表示
+	@GetMapping("create")
+	public String Create(Model model) {
+		//		MyBooksModel mybooks = new MyBooksModel();
+		//		model.addAttribute("myBook", mybooks);
+		return "create";
+	}
+
+	//変更画面の表示
+	//		@GetMapping("{id}/edit")
+	//		public String edit(@PathVariable Integer id, @ModelAttribute("mybook") MyBooksModel myBooksModel, Model model) {
+	//			model.addAttribute("mybook", myBooksService.selectOne(id));
+	//			return "edit";
+	//		}
+
+	//新規作成画面の挿入処理　エラーで戻り、成功したら一覧へ画面遷移
+	//		@PostMapping
+	//		public String postBooksCreate(@ModelAttribute("mybook") @Validated MyBooksModel myBooksModel,
+	//				BindingResult bindingResult) {
+	//			if (bindingResult.hasErrors()) {
+	//				return "new";
+	//			}
+	//			myBooksService.insert(myBooksModel);
+	//			return "redirect:/";
+	//		}
+
+	//登録情報を更新画面へ　入力なければ、エラー出て戻り、更新したら一覧画面へ値が更新されて戻る
+	//		@PutMapping("{id}") //POSTでもいけるかもです
+	//		public String updateBooks(@PathVariable Integer id, @ModelAttribute("mybook") @Validated MyBooksModel myBooksModel,
+	//				BindingResult bindingResult, Model model) {
+	//			if (bindingResult.hasErrors()) {
+	//				model.addAttribute("mybook", myBooksModel);
+	//				return "edit";
+	//			} else {
+	//				myBooksModel.setId(id);
+	//				myBooksService.update(myBooksModel);
+	//				return "redirect:/";
+	//			}
+	//		}
+
+	//本の情報削除します
+	//		@DeleteMapping("{id}")
+	//		public String delete(@PathVariable Integer id) {
+	//			myBooksService.delete(id);
+	//			return "redirect:/";
+	//		}
+
+}
