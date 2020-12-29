a3b4a2676f23ab575bf0944b60e031f598885ba6
Ryuta Kuwamura <staticjbulejass@gmail.com>
2020-12-29 11:55:18 +0900

    日付計算式を追加

--- DateCalculationVK-1/src/main/java/com/example/dateapp/service/CalculationService.java
+++ DateCalculationVK-1/src/main/java/com/example/dateapp/service/CalculationService.java
@@ -1,5 +1,7 @@
 package com.example.dateapp.service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,17 +17,19 @@ public class CalculationService {
 	@Autowired
 	private DateFormulaRepository repository;
 
-	//日付計算式の一覧を全件検索して取得する
+	//日付計算式一覧を取得する
+	//@return 日付計算式の全件
 
 	public List<DateFormula> search() {
 		return repository.select();
 	}
 
 	// 日付計算式を取得します。
+	//@return 日付計算式
 
-	//	public DateFormula search(String dateId) {
-	//		return repository.selectID(dateId);
-	//	}
+	public DateFormula search(String dateId) {
+		return repository.selectID(dateId);
+	}
 
 	//日付計算式を登録します。
 
@@ -44,5 +48,23 @@ public class CalculationService {
 	//	public void delete(String dateId) {
 	//		repository.delete(dateId);
 	//	}
+
+	/**
+	 * 日付計算を実行するクラス
+	 * 計算基準日をベースに、日付計算式の加減値に基づいて計算を行う。
+	 * 計算語の書式は「yyyyMMdd」の文字列となります。
+	 * @param baseDate 計算基準日
+	 * @param formula 日付計算式
+	 * @return 計算結果
+	 */
+
+	public String calculate(String baseDate, DateFormula formula) {
+		LocalDate dete = LocalDate.parse(baseDate, DateTimeFormatter.ofPattern("yyyyMMdd"));
+		LocalDate calulatedDate = dete.plusYears(formula.getAdjustmentYear())
+				.plusMonths(formula.getAdjustmentMonth())
+				.plusDays(formula.getAdjustmentDay());
+		return calulatedDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+
+	}
 
 }
