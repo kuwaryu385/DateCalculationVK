652ad70f579ae5ca9f928495a68a54c1d695407d
Ryuta Kuwamura <staticjbulejass@gmail.com>
2021-01-04 11:07:33 +0900

    計算式の更新機能の追加　XML　マッパークラス

--- DateCalculationVK-1/src/main/java/com/example/dateapp/repository/DateFormulaRepository.java
+++ DateCalculationVK-1/src/main/java/com/example/dateapp/repository/DateFormulaRepository.java
@@ -6,26 +6,49 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.example.dateapp.domain.DateFormula;
 
+/**
+ * 日付計算式を扱うリポジトリ
+ * ORMクラス。
+ * @author kuwamura
+ *
+ */
+
 @Mapper
 public interface DateFormulaRepository {
 
-	//	日付計算式の一覧を取得する
+	/**
+	 * 日付計算式の一覧を取得する。
+	 * @return 日付計算式の一覧
+	 */
 
 	List<DateFormula> select();
 
-	//	 日付計算式を取得する
+	/**
+	 * 日付計算式を取得する
+	 * @param dateId
+	 * @return 日付計算式
+	 */
 
 	DateFormula selectId(String dateId);
 
-	//	日付計算式を新規登録する
+	/**
+	 * 	日付計算式を新規登録する
+	 * @param formula
+	 */
 
 	void insert(DateFormula formula);
 
-	//	 日付計算式を更新します。
+	/**
+	 * 日付計算式を更新します。
+	 * @param formula
+	 */
 
 	void update(DateFormula formula);
 
-	//	 日付計算式を削除します。
+	/**
+	 *  日付計算式を削除します。
+	 * @param dateId
+	 */
 
 	void delete(String dateId);
 
