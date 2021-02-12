235b2383befa918968e56a20170c34511de98e73
Ryuta Kuwamura <staticjbulejass@gmail.com>
2021-02-12 10:44:00 +0900

    設定変更

--- DateCalculationVK-1/src/main/resources/schema.sql
+++ DateCalculationVK-1/src/main/resources/schema.sql
@@ -1,9 +1,9 @@
 --CREATE DATABASE IF NOT EXISTS同じ名前のデータベースがあっても複製できるよー
-CREATE DATABASE IF NOT EXISTS `datecalculationvk`;
+--CREATE DATABASE IF NOT EXISTS `datecalculationvk`;
 --データベースを指定
-USE `datecalculationvk`;
+--USE `datecalculationvk`;
 -- テーブルが存在していればテーブルを消す
-DROP TABLE IF EXISTS `dateformula`;
+--DROP TABLE IF EXISTS `dateformula`;
 --同じテーブル名があっても複製できるよー
 CREATE TABLE IF NOT EXISTS dateformula(
           dateId          varchar  (6)               NOT NULL  PRIMARY KEY
